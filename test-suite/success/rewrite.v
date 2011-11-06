mattam82/Coq-misc

commit 28c039b09a6bafba25226383a99823cb2e2085be

Added full pattern-unification on Meta for tactic unification. No way to control it yet; maybe flag use_evars_pattern_unification should be generalized for that purpose.
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@14199 85f007b7-540e-0410-9357-904b9bb8a0f7

pretyping/evarutil.ml
@@ -1285,8 +1285,8 @@ let is_unification_pattern_evar env (_,args) l t =
 let is_unification_pattern (env,nb) f l t =
   match kind_of_term f with
     | Meta _ ->
-	array_for_all (fun c -> isRel c && destRel c <= nb) l
-	&& array_distinct l
+	let dummy_ev = (f,[||]) in
+	is_unification_pattern_evar env dummy_ev (Array.to_list l) t
     | Evar ev ->
 	is_unification_pattern_evar env ev (Array.to_list l) t
     | _ ->

pretyping/unification.ml
@@ -231,6 +231,9 @@ let elim_no_delta_flags = {
   allow_K_in_toplevel_higher_order_unification = true
 }
 
+let set_no_head_reduction flags =
+  { flags with restrict_conv_on_strict_subterms = true }
+
 let use_evars_pattern_unification flags =
   !global_evars_pattern_unification_flag && flags.use_evars_pattern_unification
   && Flags.version_strictly_greater Flags.V8_2
@@ -837,6 +840,14 @@ let check_types env flags (sigma,_,_ as subst) m n =
       (get_type_of env sigma n)
   else subst
 
+let try_resolve_typeclasses env evd flags m n =
+  if flags.resolve_evars then
+    try Typeclasses.resolve_typeclasses ~onlyargs:false ~split:false
+	  ~fail:true env evd
+    with e when Typeclasses_errors.unsatisfiable_exception e ->
+      error_cannot_unify env evd (m, n)
+  else evd
+
 let w_unify_core_0 env with_types cv_pb flags m n evd =
   let (mc1,evd') = retract_coercible_metas evd in
   let (sigma,ms,es) = check_types env flags (evd,mc1,[]) m n in
@@ -844,15 +855,18 @@ let w_unify_core_0 env with_types cv_pb flags m n evd =
      unify_0_with_initial_metas (evd',ms,es) false env cv_pb flags m n
   in
   let evd = w_merge env with_types flags subst2 in
-    if flags.resolve_evars then
-      try Typeclasses.resolve_typeclasses ~onlyargs:false ~split:false
-	~fail:true env evd
-      with e when Typeclasses_errors.unsatisfiable_exception e ->
-	error_cannot_unify env evd (m, n)
-    else evd
+  try_resolve_typeclasses env evd flags m n
 
 let w_unify_0 env = w_unify_core_0 env false
 let w_typed_unify env = w_unify_core_0 env true
+
+let w_typed_unify_list env cv_pb flags f1 l1 f2 l2 evd =
+  let flags' = { flags with resolve_evars = false } in
+  let f1,l1,f2,l2 = adjust_app_list_size f1 l1 f2 l2 in
+  let evd =
+    List.fold_left2 (fun evd m n ->
+      w_unify_core_0 env true cv_pb flags' m n evd) evd (f1::l1) (f2::l2) in
+  try_resolve_typeclasses env evd flags (applist(f1,l1)) (applist(f2,l2))
 
 (* takes a substitution s, an open term op and a closed term cl
    try to find a subterm of cl which matches op, if op is just a Meta
@@ -1068,7 +1082,7 @@ let w_unify env cv_pb ?(flags=default_unify_flags) ty1 ty2 evd =
       | (Meta _, true, Lambda _, _ | Lambda _, _, Meta _, true)
 	  when List.length l1 = List.length l2 ->
 	  (try
-	      w_typed_unify env cv_pb flags ty1 ty2 evd
+	      w_typed_unify_list env cv_pb flags hd1 l1 hd2 l2 evd
 	    with ex when precatchable_exception ex ->
 	      try
 		w_unify2 env flags cv_pb ty1 ty2 evd
@@ -1081,7 +1095,7 @@ let w_unify env cv_pb ?(flags=default_unify_flags) ty1 ty2 evd =
 	    with PretypeError (env,_,NoOccurrenceFound _) as e -> raise e
 	      | ex when precatchable_exception ex ->
 		  try
-		    w_typed_unify env cv_pb flags ty1 ty2 evd
+		    w_typed_unify_list env cv_pb flags hd1 l1 hd2 l2 evd
 		  with ex' when precatchable_exception ex' ->
 		    raise ex)
 

pretyping/termops.ml
@@ -282,6 +282,16 @@ let decompose_app_vect c =
   | App (f,cl) -> (f, cl)
   | _ -> (c,[||])
 
+let adjust_app_list_size f1 l1 f2 l2 =
+  let len1 = List.length l1 and len2 = List.length l2 in
+  if len1 = len2 then (f1,l1,f2,l2)
+  else if len1 < len2 then
+   let extras,restl2 = list_chop (len2-len1) l2 in
+    (f1, l1, applist (f2,extras), restl2)
+  else
+    let extras,restl1 = list_chop (len1-len2) l1 in
+    (applist (f1,extras), restl1, f2, l2)
+
 (* [map_constr_with_named_binders g f l c] maps [f l] on the immediate
    subterms of [c]; it carries an extra data [l] (typically a name
    list) which is processed by [g na] (which typically cons [na] to

pretyping/termops.mli
@@ -201,6 +201,9 @@ val last_arg : constr -> constr
 (** Force the decomposition of a term as an applicative one *)
 val decompose_app_vect : constr -> constr * constr array
 
+val adjust_app_list_size : constr -> constr list -> constr -> constr list ->
+  (constr * constr list * constr * constr list)
+
 (** name contexts *)
 type names_context = name list
 val add_name : name -> names_context -> names_context

test-suite/success/unification.v
@@ -21,6 +21,12 @@ Proof.
 intros; apply H.
 Qed.
 
+  (* Feature introduced June 2011 *)
+
+Lemma l7 : forall x (P:nat->Prop), (forall f, P (f x)) -> P (x+x).
+Proof.
+intros x P H; apply H.
+Qed.
 
 (* Example submitted for Zenon *)
 
@@ -138,3 +144,4 @@ Goal (forall (A B : Set) (f : A -> B), (fun x => f x) = f) ->
 Proof.
   intros.
   rewrite H with (f:=f0).
+Abort.

commit 86b94ba2bef2c30310d18c5cbc12e8dc89e9b178

Fixing incorrect change to pattern-unification over Meta's introduced in r14199 (June 2011). Meta's implicitly depend on the context they are defined in and this has to be taken into account for checking if occurrences are distinct (in particular, no Var's are allowed as arguments of a pattern-unifiable Meta). The example expected to be accepted thanks to r14199 is not a pattern-unification problem (it has more than one solution) and was anyway already accepted (strange).
Compared to before r14199, aliases expansion and restriction of
pattern unification check to variables occurring in the right-hand
side are however now taken into account.

git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@14642 85f007b7-540e-0410-9357-904b9bb8a0f7

pretyping/evarconv.ml
@@ -227,7 +227,7 @@ and evar_eqappr_x ts env evd pbty (term1,l1 as appr1) (term2,l2 as appr2) =
 
     | Flexible ev1, MaybeFlexible flex2 ->
 	let f1 i =
-          match is_unification_pattern env term1 l1 (applist appr2) with
+          match is_unification_pattern_evar env ev1 l1 (applist appr2) with
           | Some l1' ->
 	    (* Miller-Pfenning's patterns unification *)
 	    (* Preserve generality (except that CCI has no eta-conversion) *)
@@ -260,7 +260,7 @@ and evar_eqappr_x ts env evd pbty (term1,l1 as appr1) (term2,l2 as appr2) =
 
     | MaybeFlexible flex1, Flexible ev2 ->
 	let f1 i =
-	  match is_unification_pattern env term2 l2 (applist appr1) with
+	  match is_unification_pattern_evar env ev2 l2 (applist appr1) with
           | Some l1' ->
 	    (* Miller-Pfenning's patterns unification *)
 	    (* Preserve generality (except that CCI has no eta-conversion) *)
@@ -358,7 +358,7 @@ and evar_eqappr_x ts env evd pbty (term1,l1 as appr1) (term2,l2 as appr2) =
 	     evar_conv_x ts (push_rel (na,None,c) env) i CONV c'1 c'2)]
 
     | Flexible ev1, (Rigid _ | PseudoRigid _) ->
-	(match is_unification_pattern env term1 l1 (applist appr2) with
+	(match is_unification_pattern_evar env ev1 l1 (applist appr2) with
         | Some l1 ->
 	  (* Miller-Pfenning's pattern unification *)
 	  (* Preserve generality thanks to eta-conversion) *)
@@ -372,7 +372,7 @@ and evar_eqappr_x ts env evd pbty (term1,l1 as appr1) (term2,l2 as appr2) =
 	  true)
 
     | (Rigid _ | PseudoRigid _), Flexible ev2 ->
-	(match is_unification_pattern env term2 l2 (applist appr1) with
+	(match is_unification_pattern_evar env ev2 l2 (applist appr1) with
         | Some l2 ->
 	  (* Miller-Pfenning's pattern unification *)
 	  (* Preserve generality thanks to eta-conversion) *)

pretyping/evarutil.ml
@@ -1335,30 +1335,39 @@ let get_actual_deps aliases l t =
 
 (* Check if an applied evar "?X[args] l" is a Miller's pattern *)
 
-let find_unification_pattern_args env args l t =
+let find_unification_pattern_args env l t =
   if List.for_all (fun x -> isRel x || isVar x) l (* common failure case *) then
     let aliases = make_alias_map env in
-    let l' = Array.to_list args @ l in
-    match (try Some (expand_and_check_vars aliases l') with Exit -> None) with
-    | Some l when constr_list_distinct (get_actual_deps aliases l t) ->
-        Some (list_skipn (Array.length args) l)
-    | _ ->
-      None
+    match (try Some (expand_and_check_vars aliases l) with Exit -> None) with
+    | Some l as x when constr_list_distinct (get_actual_deps aliases l t) -> x
+    | _ -> None
   else
     None
 
-let is_unification_pattern env f l t =
+let is_unification_pattern_meta env nb m l t =
+  (* Variables from context and rels > nb are implicitly all there *)
+  (* so we need to be a rel <= nb *)
+  if List.for_all (fun x -> isRel x && destRel x <= nb) l then
+    match find_unification_pattern_args env l t with
+    | Some _ as x when not (dependent (mkMeta m) t) -> x
+    | _ -> None
+  else
+    None
+
+let is_unification_pattern_evar env (evk,args) l t =
+  if List.for_all (fun x -> isRel x || isVar x) l (* common failure case *) then
+    let n = Array.length args in
+    match find_unification_pattern_args env (Array.to_list args @ l) t with
+    | Some l when not (occur_evar evk t) -> Some (list_skipn n l)
+    | _ -> None
+  else
+    None
+
+let is_unification_pattern (env,nb) f l t =
   match kind_of_term f with
-    | Meta m ->
-        (match find_unification_pattern_args env [||] l t with
-        | Some _ as x when not (dependent f t) -> x
-        | _ -> None)
-    | Evar (evk,args) ->
-        (match find_unification_pattern_args env args l t with
-        | Some _ as x when not (occur_evar evk t) -> x
-        | _ -> None)
-    | _ ->
-	None
+  | Meta m -> is_unification_pattern_meta env nb m l t
+  | Evar ev -> is_unification_pattern_evar env ev l t
+  | _ -> None
 
 (* From a unification problem "?X l = c", build "\x1...xn.(term1 l2)"
    (pattern unification). It is assumed that l is made of rel's that

pretyping/evarutil.mli
@@ -96,7 +96,10 @@ val define_evar_as_product : evar_map -> existential -> evar_map * types
 val define_evar_as_lambda : env -> evar_map -> existential -> evar_map * types
 val define_evar_as_sort : evar_map -> existential -> evar_map * sorts
 
-val is_unification_pattern : env -> constr -> constr list ->
+val is_unification_pattern_evar : env -> existential -> constr list ->
+  constr -> constr list option
+
+val is_unification_pattern : env * int -> constr -> constr list ->
   constr -> constr list option
 
 val evar_absorb_arguments : env -> evar_map -> existential -> constr list ->

pretyping/unification.ml
@@ -437,13 +437,13 @@ let unify_0_with_initial_metas (sigma,ms,es as subst) conv_at_top env cv_pb flag
 	| App (f1,l1), _ when
 	    (isMeta f1 && use_metas_pattern_unification flags nb l1
              || use_evars_pattern_unification flags && isAllowedEvar flags f1) &
-	    store whenmem (is_unification_pattern curenv f1 (Array.to_list l1) cN) ->
+	    store whenmem (is_unification_pattern curenvnb f1 (Array.to_list l1) cN) ->
 	      solve_pattern_eqn_array curenvnb f1 (restore whenmem) cN substn
 
 	| _, App (f2,l2) when
 	    (isMeta f2 && use_metas_pattern_unification flags nb l2
              || use_evars_pattern_unification flags && isAllowedEvar flags f2) &
-	    store whenmem (is_unification_pattern curenv f2 (Array.to_list l2) cM) ->
+	    store whenmem (is_unification_pattern curenvnb f2 (Array.to_list l2) cM) ->
 	      solve_pattern_eqn_array curenvnb f2 (restore whenmem) cM substn
 
 	| App (f1,l1), App (f2,l2) ->

test-suite/success/rewrite.v
@@ -118,4 +118,14 @@ Axiom s : forall (A B : Type) (p : A * B), p = (fst p, snd p).
 Definition P := (nat * nat)%type.
 Goal forall x:P, x = x.
 intros. rewrite s.
+Abort.
+
+(* Test second-order unification and failure of pattern-unification *)
+
+Goal forall (P: forall Y, Y -> Prop) Y a, Y = nat -> (True -> P Y a) -> False.
+intros.
+(* The next line used to succeed between June and November 2011 *)
+(* causing ill-typed rewriting *)
+Fail rewrite H in H0.
+Abort.
 